88f8d81956ecafca96de3edc56751ea3e3bb5c0d
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2020-06-20 19:04:59 +0100

    feat: Add babel and configs

---
+++ babel.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@config': './src/config',
+          '@controllers': './src/controllers',
+          '@models': './src/models',
+          '@views': './src/views',
+        },
+      },
+    ],
+  ],
+  ignore: ['**/*.spec.ts'],
+}
